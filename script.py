300b3e4fe7c3fd3efc2b09919be422cbcdc52528
NavonilDas <navneeladas@gmail.com>
2018-08-18 11:45:22 +0530

    Added to read images and seprate them into training and testing data

--- script.py
+++ script.py
@@ -1,13 +1,15 @@
 import cv2
 import os
-# import numpy as np
-# import random
+import numpy as np
+import random
 # import tensorflow as tf
 # import tqdm as tqdm
 # import matplotlib.pyplot as plt
 
 daisy = "/flowers/daisy"
 roses = "/flowers/roses"
+SIZE = 128
+RATIO = 0.7
 # cd = os.getcwd()
 
 daisy_files = [] # Files Containing on the Daisy Folder
@@ -20,8 +22,27 @@ def read_directories():
     roses_files = os.listdir('.' + roses) 
 
 def import_files():
+    all_data = [] # all the data daisy + roses in grayscale format
+    training_data = []
+    testing_data = []
+    # DAISY ===> Label = 1
+    for dsy in daisy_files:
+        img = cv2.imread('.' + daisy + '/' + dsy ,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img,(SIZE,SIZE))
+        all_data.append([np.array(img),1])
     
-    return 0
+    # ROSES ===> Label = 2
+    for ros in roses_files:
+        img = cv2.imread('.' + roses + '/' + ros ,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img,(SIZE,SIZE))
+        all_data.append([np.array(img),2])
+    
+    random.shuffle(all_data)
+    train_count = int(len(all_data) * RATIO)
+    training_data = all_data[:train_count]
+    testing_data = all_data[train_count:]
+    return training_data,testing_data
 
 # print(os.getcwd())
 read_directories()
+import_files()
